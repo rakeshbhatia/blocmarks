b8f398df7c3b44bda07e7c439d230fb37167f1bf
Rakesh <rakeshbhatia87@gmail.com>
2016-07-13 15:06:40 -0700

    Checkpoint 4 topics crud: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,5 +8,31 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.title = params[:topic][:title]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
   end
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -37,6 +37,16 @@ RSpec.describe TopicsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "initializes @topic" do
+      get :new
+      expect(assigns(:topic)).not_to be_nil
+    end
   end
 
   describe "GET #edit" do
@@ -46,4 +56,32 @@ RSpec.describe TopicsController, type: :controller do
     end
   end
 
+  describe "POST create" do
+    it "increases the number of topics by 1" do
+      expect{ post :create, {topic: {title: RandomData.random_sentence}}}.to change(Topic,:count).by(1)
+    end
+
+    it "assigns Topic.last to @topic" do
+      post :create, {topic: {title: RandomData.random_sentence}
+      expect(assigns(:topic)).to eq Topic.last
+    end
+
+    it "redirects to the new topic" do
+      post :create, {topic: {title: RandomData.random_sentence}}
+      expect(response).to redirect_to Topic.last
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the topic" do
+      delete :destroy, {id: my_topic.id}
+      count = Topic.where({id: my_topic.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to topics index" do
+      delete :destroy, {id: my_topic.id}
+      expect(response).to redirect_to topics_path
+    end
+  end
 end
